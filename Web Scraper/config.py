79dcd156a4dd886d050bf0fc310dce175ccbf99e
Andrew Wu <andrew2002wu@gmail.com>
2024-02-01 16:01:49 -0800

    change config to windows kernel

---
+++ Web Scraper/config.py
@@ -0,0 +1,10 @@
+# Change 'control_key': 'ctrl' for Windows
+# Change #'control_key': 'command' for mac
+keys = {
+   'server' : "lmusqldb.database.windows.net",
+   'database' : "LMUSQLDB",
+   'username' : "lmuuser",
+   'password' : "dcH8PHTD9kys1986!",
+   'driver-path' :"C:\\Users\\andre\\chromedrivers\\chromedriver-win64\\chromedriver-win64",    # edit to chrome driver path
+   'control_key' : 'ctrl'
+}
